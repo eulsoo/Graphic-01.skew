040af1396669a373767bb124e6e1e5e333f80ec5
eulsoo <eulsoo.jung@gmail.com>
2022-12-15 12:06:17 +0900

    [skew.js] 2022.12.15-12.06.17

--- skew.js
+++ skew.js
@@ -11,41 +11,48 @@ const settings = {
 
 const sketch = ({ context, width, height }) => {
   let x, y, w, h, fill, stroke, blend;
-  const num = 90;
+  const num = 150;
   const degrees = -30;
   const rects = [];
   const rectColors = [
     random.pick(risoColors),
-    random.pick(risoColors),
     random.pick(risoColors)
   ];
-
   const bgColor = random.pick(risoColors).hex;
+  const mask = {
+    radius: width * 0.4,
+    sides: 3,
+    x: width * 0.5,
+    y: height * 0.6
+  }
 
   for (let i = 0; i < num; i++) {
     // 기본크기를 정한다.
     x = random.range(0, width); 
     y = random.range(0, height); 
-    w = random.range(200, 600); 
+    w = random.range(300, 500); 
     h = random.range(40, 200); 
-    // fill = `rgba(${random.range(0, 255)}, 0, ${random.range(0, 255)}, ${random.range(0.1, 1)})`;
     fill = random.pick(rectColors).hex;
     stroke = random.pick(rectColors).hex;
     blend = (random.value() > 0.5) ? 'overlay' : 'source-over';
-
-    rects.push({x, y, w, h, fill, stroke})
+    rects.push({x, y, w, h, fill, stroke});
   }
 
   return ({ context, width, height }) => {
-    
     context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
+
+    context.save();
+    context.translate(mask.x, mask.y);
+    drawPolygon({ context, radius: mask.radius, sides: mask.sides });
+    context.clip(); 
 
     rects.forEach(rect => {
       const {x, y, w, h, fill, stroke, blend} = rect;
       let shadowColor;
 
       context.save(); 
+      context.translate(-mask.x, -mask.y);
       context.translate(x, y);
       context.strokeStyle = stroke;
       context.fillStyle = fill;
@@ -53,22 +60,33 @@ const sketch = ({ context, width, height }) => {
       context.globalCompositeOperation = blend;
 
       drawSkewedRect({context, w, h, degrees});
-      shadowColor = Color.offsetHSL(fill, 0, 0, -20);
-      shadowColor.rgba[3] = 0.5;
-      context.shadowColor = Color.style(shadowColor.rgba);
-      context.shadowOffsetX = -10;
-      context.shadowOffsetY = 20;
+      // shadowColor = Color.offsetHSL(fill, 0, 0, -20);
+      // shadowColor.rgba[3] = 0.5;
+      // context.shadowColor = Color.style(shadowColor.rgba);
+      // context.shadowOffsetX = -10;
+      // context.shadowOffsetY = 20;
       context.stroke();
       context.lineWidth = 2;
       context.strokeStyle = 'black';
       context.stroke();
       context.fill();
       context.shadowColor = null;
-  
       context.restore();
     });
+    context.restore();
+
+    // polygon outline
+    context.save();
+    context.translate(mask.x, mask.y);
+    drawPolygon({ context, radius: mask.radius - context.lineWidth, sides: mask.sides })
+    context.globalCompositeOperation = 'color-burn'
+    context.lineWidth = 30;
+    context.strokeStyle = rectColors[0].hex;
+    context.stroke();
+    context.restore();
   };
 };
+
 function drawSkewedRect({context, w = 600, h = 200, degrees = -45}) {
   const angle = math.degToRad(degrees); 
   // 수평을 기준으로 원하는 지점을 얻기 위해 각도를 설정하면 시작점에서
@@ -78,7 +96,6 @@ function drawSkewedRect({context, w = 600, h = 200, degrees = -45}) {
   
   context.save();
   context.translate(rx * -0.5, (ry + h) * -0.5);
-  
   context.beginPath();
   context.moveTo(0, 0);
   context.lineTo(rx, ry);
@@ -86,7 +103,17 @@ function drawSkewedRect({context, w = 600, h = 200, degrees = -45}) {
   context.lineTo(0, h);
   context.closePath();
   context.stroke();
-
   context.restore();
 }
+const drawPolygon = ({ context, radius = 100, sides = 3 }) => {
+  const slice = Math.PI * 2 / sides;
+  context.beginPath();
+  context.moveTo(0, -radius);
+
+  for (let i = 1; i < sides; i++) {
+    const theta = i * slice - Math.PI * 0.5;
+    context.lineTo(Math.cos(theta) * radius, Math.sin(theta) * radius)
+  }
+  context.closePath();
+};
 canvasSketch(sketch, settings);
